ee7e6573994bf819fd57c258859091bf53a3b2d7
박진주 <voguemh33@naver.com>
2021-10-06 18:30:32 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+from django.db import models
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=10)
+    # 글자수의 길이가 제한된 텍스트는 CharField를 사용해야함함
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 기존 모델을 속성으로 연결하기 위해 Foreignkey 사용
+    # on_delete=models.CASCADE 답변과 연결된 질문이 삭제 될 경우 답변도 함께 삭제
+
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+
