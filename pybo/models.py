b2b40d88d6105a176d074ca372c3e25e45f72306
박진주 <voguemh33@naver.com>
2021-10-20 18:09:35 +0900

    추천기능

--- pybo/models.py
+++ pybo/models.py
@@ -6,20 +6,20 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=100)
     # 글자수의 길이가 제한된 텍스트는 CharField를 사용해야함함
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='author_answer')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # 기존 모델을 속성으로 연결하기 위해 Foreignkey 사용
     # on_delete=models.CASCADE 답변과 연결된 질문이 삭제 될 경우 답변도 함께 삭제
@@ -27,6 +27,7 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from ..models import Question, Answer, Comment
 from ..forms import QuestionForm, AnswerForm, CommentForm
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
